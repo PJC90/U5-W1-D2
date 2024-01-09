62a296070f77040d11613fad07e46f768537908d
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2024-01-09 21:53:49 +0100

    Update MyRunner.java

--- src/main/java/pierpaolo/u5w1d1/MyRunner.java
+++ src/main/java/pierpaolo/u5w1d1/MyRunner.java
@@ -13,19 +13,25 @@ public class MyRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5w1d1Application.class);
+        try{
+            Menu m = (Menu) ctx.getBean("menu");
+            m.printMenu();
 
-        Menu m = (Menu) ctx.getBean("menu");
-        m.printMenu();
+            Tavolo tavolo1 = (Tavolo) ctx.getBean("tavolo_1");
 
-        Tavolo tavolo1 = (Tavolo) ctx.getBean("tavolo_1");
+            List<Prodotto> prodottiOrdinati = new ArrayList<>();
+            prodottiOrdinati.add(ctx.getBean("vikinga", Pizza.class));
+            prodottiOrdinati.add(ctx.getBean("coca", Bevanda.class));
 
-        List<Prodotto> prodottiOrdinati = new ArrayList<>();
-        prodottiOrdinati.add(ctx.getBean("vikinga", Pizza.class));
-        prodottiOrdinati.add(ctx.getBean("coca", Bevanda.class));
+            Ordine ordine1 = new Ordine(4, prodottiOrdinati, tavolo1);
+            System.out.println(ordine1);
+            System.err.println("Conto: " + ordine1.getTotale());
+        } catch (Exception ex){
+            System.err.println(ex.getMessage());
+        } finally {
+            ctx.close();
+        }
 
-        Ordine ordine1 = new Ordine(4, prodottiOrdinati, tavolo1);
-        System.out.println(ordine1);
 
-        ctx.close();
     }
 }
